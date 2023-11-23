aeea5d49e92d09316ca57f938d4a75e107ff34f2
Regina Esparza <resparza439@gmail.com>
2023-11-22 23:09:26 -0300

    router link created

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,14 @@ export class RegisterComponent {
   
   model: any = {}
 
-  constructor(private accountService:AccountService){}
+  constructor(private accountService:AccountService, private toaster:ToastrService){}
 
   register (){
     this.accountService.register(this.model).subscribe({
       next: () => {
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toaster.error(error.error)
     })
   }
 
