1a023408f77afecc9dd719a9189a4029943473d9
Regina Esparza <resparza439@gmail.com>
2024-01-10 22:51:35 -0300

    incorrect property deleted from AppUser class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,7 +5,6 @@ public class AppUser
 {
     public int Id { get; set; }
     public string UserName { get; set; }    
-    public string PhotoUrl {get; set;}
     public byte[] PasswordHash {get; set;}
     public byte[] PasswordSalt {get; set;}
     public DateOnly DateOfBirth {get; set;}
