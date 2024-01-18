9248dae798240192034d5bb6b0e51736b6955060
Regina Esparza <resparza439@gmail.com>
2024-01-17 23:11:29 -0300

    UserLike Entity created

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,26 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions options) : base(options)
     {
     }
-    public DbSet<AppUser> Users { get; set; }
+    public DbSet<AppUser> Users {get; set;}
+    public DbSet<UserLike> Likes {get; set;}
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserLike>()
+            .HasKey(k => new {k.SourceUserId, k.TargerUserId});
+
+        modelBuilder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+       
+        modelBuilder.Entity<UserLike>()
+            .HasOne(s => s.TargerUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargerUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }   
 }
