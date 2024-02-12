e9654cdd405374d413ccd56102cbac49b14c93d0
Regina Esparza <resparza439@gmail.com>
2024-02-12 01:13:22 -0300

    microsoft aspnet Identity added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,12 +32,6 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-   
-            user.UserName = registerDto.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;            
-            
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
@@ -58,15 +52,6 @@ namespace API.Controllers
                 x => x.UserName == loginDto.UserName);
 
             if(user == null) return Unauthorized("invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computeHash.Length; i++){
-
-                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
-            }
                 
            return new UserDTO
             {
