93dc02a3e60d17efc209ff33162aa8283cbae38e
Regina Esparza <resparza439@gmail.com>
2024-02-13 22:40:39 -0300

    add roles to admin methods

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -30,7 +30,16 @@ namespace API.Extensions
                         ValidateIssuer = false,
                         ValidateAudience = false    
                     };
-                });
+                }
+            );
+
+            services.AddAuthorization(opt => 
+                {
+                    opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                    opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                }
+            );
+
             return services;
         }
     }
