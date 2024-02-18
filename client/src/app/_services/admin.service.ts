8db9d68bbfc51c3bc0289d6db8dc63db6ed5de42
Regina Esparza <resparza439@gmail.com>
2024-02-17 23:45:06 -0300

    update users roles through roles-modal-component implemented

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -12,6 +12,11 @@ export class AdminService {
   constructor(private http: HttpClient) { }
 
   getUserWithRoles(){
-    return this.http.get<User[]>(this.baseUrl + 'admin/user-with-roles');
+    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+  }
+
+  updateUserRoles(username: string, roles: any){
+    return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/'
+     + username + '?roles=' + roles, {});
   }
 }
